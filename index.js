c16181f1838ce15f97269bed2e327c7ce6752e3e
trent <trntannan1@gmail.com>
2024-02-14 10:24:13 +1300

    newest

--- index.js
+++ index.js
@@ -1,36 +1,88 @@
-// declare and log
-let favouriteFood = "pizza";
-console.log(favouriteFood)
+// // declare and log
 
-// basic comparison
-let age = 20;
-if (age >= 18) {
-    age = "diff number"
-}
-console.log(age)
+// let favouriteFood = "pizza";
+// console.log(favouriteFood)
 
-// the guessing game
-let secretNumber = 22;
-let userGuess = prompt("Guess the secret Number");
-if (userGuess == secretNumber) {
-    console.log("correct")
-} else {
-    console.log("incorrect")
-}
+// // basic comparison
+
+// let age = 20;
+// if (age >= 18) {
+//     age = "diff number"
+// }
+// console.log(age)
 
 
-// simple math test
-let userNumberOne = prompt ("provide a number");
-let userNumberTwo = prompt ("provide a different number for userNumberTwo");
-let sum = userNumberOne + userNumberTwo;
-console.log(sum);
 
-// whats the weather
-let temperature = 24;
-if (temperature <= 0){
-    console.log("cold")
-} else if (temperature > 0 && temperature <= 20) {
-    console.log("warm")
-} else {
-    console.log("hot")
-}
+// // simple math test
+
+// let userNumberOne = prompt ("provide a number");
+// let userNumberTwo = prompt ("provide a different number for userNumberTwo");
+// let sum = userNumberOne + userNumberTwo;
+// console.log(sum);
+
+// // whats the weather
+
+// let temperature = 24;
+// if (temperature <= 0){
+//     console.log("cold")
+// } else if (temperature > 0 && temperature <= 20) {
+//     console.log("warm")
+// } else {
+//     console.log("hot")
+// }
+
+// // age group
+
+// let age=35
+// if (age <= 13){
+//     console.log("child")
+// } else if (age > 14 && age <= 18){
+//     console.log(teenager)
+// } else {
+//     console.log("adult")
+// }
+
+// // odd and even
+
+// let userNumber = prompt ("provide a number")
+// if (userNumber % 2 == 0){
+//     console.log("even number")
+// } else {
+//     console.log("odd number")
+// }
+
+// // build a sentence
+
+// let subject = "subject"
+// let verb =  "verb"
+// let object = "object"
+//  console.log ("subject "+ "verb "+ "object ")
+
+// // the guessing game
+
+// let secretNumber = 8;
+// let userGuess = prompt("Guess the secret Number");
+// if (userGuess == secretNumber) {
+//     console.log("Congratulations!")
+// } else {
+//     console.log("TRy again!")
+
+// }
+
+
+
+// // your age in days
+
+// let age = 35;
+
+// // one way
+// // function convertAgeToDays(age) {
+// //     return age * 365;
+// // }
+
+// let ageInDays = convertAgeToDays(age);
+
+//  console.log(ageInDays);
+
+
+
